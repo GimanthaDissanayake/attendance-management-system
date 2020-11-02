f53cf93de97f4441c8c06a60e2c5d95f66c72986
Gimantha <gimanthad@gmail.com>
2020-11-02 07:44:33 +0530

    Added Footer

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,27 @@
     <v-content class="mx-4 mb-4">
       <router-view></router-view>
     </v-content>
-    <Footer />
+    <v-footer
+      absolute
+      class="font-weight-medium"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Group 3</strong>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
 
 export default {
-  components: { Navbar, Footer },
-  name: 'App',
-  data () {
-    return {
-      
-    }
-  }
+  components: { 
+    Navbar 
+  },
+  name: 'App'
 }
 </script>
