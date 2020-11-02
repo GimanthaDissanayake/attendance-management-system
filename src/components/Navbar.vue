500ac72ec23e4dec68d9493466a95ea092d627c7
Gimantha <gimanthad@gmail.com>
2020-11-02 11:58:58 +0530

    navbar update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -45,7 +45,10 @@
         >
           <v-list-item
             v-for="item in items"
-            v-bind:key="item.title">
+            v-bind:key="item.title"
+            link
+            v-show="item.access=='all' || item.access.includes('lecturer')"
+          >
               <v-list-item-icon>
                 <v-icon>{{ item.icon }}</v-icon>
               </v-list-item-icon>
@@ -56,67 +59,7 @@
         </v-list>
       </v-navigation-drawer>
   </nav>
-  <!-- <nav>
-    <v-toolbar flat app>
-      <v-toolbar-side-icon @click="drawer = !drawer" class="grey--text"></v-toolbar-side-icon>
-      <v-toolbar-title class="text-uppercase grey--text">
-        <span class="font-weight-light">face</span>
-        <span>que</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <div class="text-center">
-    <v-btn
-      class="mx-2"
-      fab
-      small
-      color="primary"
-    >
-      <v-icon >
-        mdi-android-messages
-      </v-icon>
-    </v-btn>
-
-    <v-btn
-      class="mx-2"
-      fab
-      small
-      color="primary"
-    >
-      <v-icon >
-        mdi-bell
-      </v-icon>
-    </v-btn>
-     <v-avatar
-      color="primary"
-      size="49"
-    >
-    <span class="white--text headline">HJ</span>
-    </v-avatar>
-    
-
-    </div>
-    
-      
-    </v-toolbar>
-
-    <v-navigation-drawer app v-model="drawer" class="primary">
-      <v-layout column align-center>
-        <h1 class="lg-3">Logo</h1>
-        <p  class="white--text">Attendance Management System</p>
-      </v-layout>
-      <v-list>
-        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
-          <v-list-tile-action>
-            <v-icon class="white--text">{{ link.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
-  </nav> -->
+  
 </template>
 
 <script>
@@ -125,16 +68,15 @@ export default {
     return {
       drawer: false,
       items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard', path: '', access: ''},
-        { title: 'Courses', icon: 'mdi-school', path: '', access: ''},
-        { title: 'Mentor', icon: 'mdi-timetable', path: '', access: ''},
-        { title: 'Timetable', icon: 'mdi-timetable', path: '', access: ''},
-      ],
-      // links: [
-      //   { icon: 'mdi-school', text: 'Courses', route: '/Courses' },
-      //   { icon: 'mdi-timetable', text: 'Timetable', route: '/Timetable' },
-      //   { icon: 'mdi-medical-bag', text: 'Medical', route: '/Medical' },
-      // ]
+        { title: 'Dashboard', icon: 'mdi-view-dashboard', path: '', access: 'all'},
+        { title: 'Courses', icon: 'mdi-school', path: '', access: ['student','lecturer','mentor','dh','admin']},
+        { title: 'Mentor', icon: 'mdi-account-supervisor', path: '', access: ['student','dh']},
+        { title: 'Timetable', icon: 'mdi-timetable', path: '', access: ['student','lecturer','mentor']},
+        { title: 'Mahapola', icon: 'mdi-cash', path: '', access: 'student'},
+        { title: 'Students', icon: 'mdi-account', path: '', access: ['lecturer','mentor','dh','admin']},
+        { title: 'Mahapola Duration', icon: 'mdi-calendar-edit', path: '', access: 'admin'},
+        { title: 'Attendance Percentage', icon: 'mdi-brightness-percent', path: '', access: 'admin'},
+      ]
     }
   }
 }
