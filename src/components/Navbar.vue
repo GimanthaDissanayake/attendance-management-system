9d499548d8f768a1b73f65619b984c788b7e21ba
Gimantha <gimanthad@gmail.com>
2020-11-02 10:00:55 +0530

    added basic navbar structure

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,62 @@
 <template>
   <nav>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <!-- add logo here -->
+        Attendance Management System
+      </v-toolbar-title>
+       <v-spacer></v-spacer>
+      <div>
+        <v-btn
+          class="mx-2"
+          fab
+          small
+          color="primary"
+        >
+          <v-icon >mdi-android-messages</v-icon>
+        </v-btn>
+        <v-btn
+          class="mx-2"
+          fab
+          small
+          color="primary"
+        >
+          <v-icon >mdi-bell</v-icon>
+        </v-btn>
+        <v-avatar
+          color="primary"
+          size="49"
+        >
+          <span class="white--text headline">SC</span>
+        </v-avatar>
+      </div>
+    </v-app-bar>
+      <v-spacer></v-spacer>
 
+      <v-navigation-drawer 
+        v-model="drawer"
+        absolute
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item
+            v-for="item in items"
+            v-bind:key="item.title">
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+  </nav>
+  <!-- <nav>
     <v-toolbar flat app>
       <v-toolbar-side-icon @click="drawer = !drawer" class="grey--text"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
@@ -60,7 +116,7 @@
       </v-list>
     </v-navigation-drawer>
 
-  </nav>
+  </nav> -->
 </template>
 
 <script>
@@ -68,11 +124,17 @@ export default {
   data() {
     return {
       drawer: false,
-      links: [
-        { icon: 'mdi-school', text: 'Courses', route: '/Courses' },
-        { icon: 'mdi-timetable', text: 'Timetable', route: '/Timetable' },
-        { icon: 'mdi-medical-bag', text: 'Medical', route: '/Medical' },
-      ]
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard', path: '', access: ''},
+        { title: 'Courses', icon: 'mdi-school', path: '', access: ''},
+        { title: 'Mentor', icon: 'mdi-timetable', path: '', access: ''},
+        { title: 'Timetable', icon: 'mdi-timetable', path: '', access: ''},
+      ],
+      // links: [
+      //   { icon: 'mdi-school', text: 'Courses', route: '/Courses' },
+      //   { icon: 'mdi-timetable', text: 'Timetable', route: '/Timetable' },
+      //   { icon: 'mdi-medical-bag', text: 'Medical', route: '/Medical' },
+      // ]
     }
   }
 }
