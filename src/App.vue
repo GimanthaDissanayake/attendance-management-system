be0bd475da554237a97933836ca9e792bfa400e4
Gimantha <gimanthad@gmail.com>
2020-11-14 19:26:00 +0530

    applied margin for content

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <v-app class="grey lighten-4">
     <Navbar></Navbar>
 
-    <v-content>
+    <v-main class="mx-4 mb-4">
       <router-view></router-view>
-    </v-content>
+    </v-main>
 
     <v-footer
       absolute
