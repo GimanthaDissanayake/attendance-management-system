6ade1758e2e1fd16f6341fcab3162ba2e760656b
Gimantha <gimanthad@gmail.com>
2020-11-14 19:16:02 +0530

    Login view update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,38 +1,23 @@
 <template>
-  <v-container fluid fill-height>
+  <v-container fluid fill-height class="bg">
     <v-row align="center" justify="center">
         <v-col cols="12" sm="4" md="4">
-            <v-card class="elevation-2" max-width="50vh">
-                <v-toolbar flat>
-                    <v-toolbar-title>Sign in</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-icon>mdi-account</v-icon>
-                </v-toolbar>
+            <v-card elevation="24" outlined>
+                <v-img contain max-height="175" src="../assets/logo.png"></v-img>
+                <p class="text-h6 text-center">Attendance Management System</p>
+                <v-card-title>Sign In</v-card-title>
                 <v-card-text>
                     <v-alert v-if="error" type="error">
                         {{ error }}
                     </v-alert>
-                    <v-from>
-                        <v-text-field
-                            label="Username"
-                            autofocus
-                            @focus="error = null"
-                            prepend-icon="mdi-account"
-                            type="text"
-                            v-model="username"
-                        />
-                        <v-text-field
-                            label="Password"
-                            @focus="error = null"
-                            prepend-icon="mdi-lock"
-                            type="password"
-                            v-model="password"
-                        />
-                    </v-from>
+                    <v-form>
+                        <v-text-field label="Username" autofocus @focus="error = null" prepend-icon="mdi-account" type="text" v-model="username"/>
+                        <v-text-field label="Password" @focus="error = null" prepend-icon="mdi-lock" type="password" v-model="password"/>
+                    </v-form>
                 </v-card-text>
                  <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn block >
+                    <v-btn block class="primary">
                         Sign in
                     </v-btn>
                 </v-card-actions>
@@ -49,5 +34,8 @@ export default {
 </script>
 
 <style>
-
+.bg {
+    background-image: url('../assets/background.png');
+    background-size: cover;
+}
 </style>
