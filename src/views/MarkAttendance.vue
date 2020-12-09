ef21c160dd9c1f938ed41e93dacdca0bb3ed86e2
Gimantha <gimanthad@gmail.com>
2020-12-09 19:48:57 +0530

    MarkAttendance update

--- src/views/MarkAttendance.vue
+++ src/views/MarkAttendance.vue
@@ -23,6 +23,12 @@
       <v-stepper-items>
         <v-stepper-content step="1">        
           <v-container>
+            <v-progress-linear v-if="isLoading"
+            color="primary"
+            indeterminate
+            rounded
+            height="6"
+            ></v-progress-linear>
             <v-row wrap style="padding: 15px">
               <v-col md3 xs12>
                 Course details:
@@ -56,6 +62,7 @@
               </v-col>
             </v-row>
             <v-row wrap style="padding: 15px">
+              <v-spacer></v-spacer>
               <v-col>
                 <v-row>
                   <v-col>
@@ -89,7 +96,7 @@
               <v-spacer></v-spacer>
                 <v-btn
                   color="primary"
-                  @click="e1 = 2">
+                  @click="setRegisteredStudentData">
                     Mark attendance
                 </v-btn>
             </v-row>
@@ -97,9 +104,22 @@
         </v-stepper-content>
 
         <v-stepper-content step="2">
-          <table>
-            <tr>
-              <td >
+          <v-container>
+            <v-progress-linear v-if="isLoading"
+            color="primary"
+            indeterminate
+            rounded
+            height="6"
+            ></v-progress-linear>
+            <!-- <v-row>
+              <v-select
+              :items="devices.deviceId"
+                label="Select Camera Device"></v-select>
+            </v-row> -->
+            <v-row align="center" justify="center">
+              <v-col 
+                cols="12"
+                sm="6">
                 <vue-web-cam
                   ref="webcam"
                   :device-id="deviceId"  
@@ -109,109 +129,148 @@
                   <!--afterdevice-id @stopped="onStopped" -->
                   <!--secondevent @started="onStarted" -->
                   <!--lastevent @camera-change="onCameraChange" -->
-              </td>
-              <td>
-                  <img :src="img" width="100%" />
-              </td>
-            </tr>
-          </table>
-          <div style="margin-bottom: 35px;">
-            <v-btn        
-            @click="onCapture"
-            text>
-              <span v-if="img === null">Mark attendance</span>
-              <span v-else >Retake Image</span>
-            </v-btn> 
-            <v-btn
-              color="primary"
-              @click="e1 = 3">
-              Continue
-            </v-btn>
-          </div>        
+              </v-col>
+              <v-col 
+                cols="12"
+                sm="6">
+                  <v-img :src="img"></v-img>
+              </v-col>
+            </v-row>
+            <v-row  align="center" justify="center">
+              <v-col>
+                <v-btn
+                @click="e1=1; img=null;">
+                  Previous
+                </v-btn>
+              </v-col>
+              <v-col>
+                <v-btn   
+                class="primary"     
+                @click="onCapture"
+                text>
+                  <span v-if="img === null">Mark attendance</span>
+                  <span v-else >Retake Image</span>
+                </v-btn> 
+              </v-col>
+              <v-col>
+                <v-btn
+                  color="primary"
+                  v-if="img != null"
+                  @click="submitImage">
+                  Continue
+                </v-btn>
+              </v-col>
+            </v-row> 
+          </v-container>       
         </v-stepper-content>
 
         <v-stepper-content step="3">
           <v-container>
-            <v-layout row wrap>
-              <v-flex md3 xs12>
+            <v-progress-linear v-if="isLoading"
+            color="primary"
+            indeterminate
+            rounded
+            height="6"
+            ></v-progress-linear>
+            <v-row>
+              <v-col>
                 Number of present students : {{ presentStudents}}
-              </v-flex>
-              <v-flex md6 xs12>
-              </v-flex>
-              <v-flex md3 xs12>
+              </v-col>
+              <v-spacer></v-spacer>
+              <v-col>
                 Attendance percentage : {{ (presentStudents / registeredStudentsLength ) * 100  }} %
-              </v-flex> 
-            </v-layout>
+              </v-col> 
+            </v-row>
             <br>
-            <v-data-table
-              :headers="headers"
-              :items="absentStudents"
-              sort-by="registraion_no"
-              class="elevation-1"
-              flat>
-              <template v-slot:top>
-                <v-toolbar
-                flat>
-                  <v-toolbar-title>Absent Students</v-toolbar-title>
-                  <v-divider
-                    class="mx-4"
-                    inset
-                    vertical></v-divider>
-                  <v-spacer></v-spacer>
-                  <v-dialog
-                    v-model="dialog"
-                    max-width="500px">
-                
-                  </v-dialog>
-                  <v-dialog v-model="dialogDelete" max-width="500px">
-                    <v-card>
-                      <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                          <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                          <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                        <v-spacer></v-spacer>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
-                </v-toolbar>
-              </template>
-              <template v-slot:item.actions="{ item }">            
-                <v-btn
-                  small
-                  @click="deleteItem(item)">
-                  <v-icon>mdi-delete</v-icon>
-                  Remove      
-                </v-btn>            
-              </template>
-            </v-data-table>
+            <v-row width=100%>
+              <v-col>
+                <v-data-table
+                  :headers="headers"
+                  :items="absentStudents"
+                  sort-by="registraion_no"
+                  class="elevation-1"
+                  flat>
+                  <template v-slot:top>
+                    <v-toolbar
+                    flat>
+                      <v-toolbar-title>Absent Students List</v-toolbar-title>
+                      <v-spacer></v-spacer>
+                      <v-dialog
+                        v-model="dialog"
+                        max-width="500px">
+                    
+                      </v-dialog>
+                      <v-dialog v-model="dialogDelete" max-width="500px">
+                        <v-card>
+                          <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+                          <v-card-actions>
+                            <v-spacer></v-spacer>
+                              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                            <v-spacer></v-spacer>
+                          </v-card-actions>
+                        </v-card>
+                      </v-dialog>
+                    </v-toolbar>
+                  </template>
+                  <template v-slot:item.actions="{ item }">            
+                    <v-btn
+                      small
+                      @click="deleteItem(item)">
+                      <v-icon>mdi-delete</v-icon>
+                      Remove      
+                    </v-btn>            
+                  </template>
+                </v-data-table>
+              </v-col>              
+            </v-row>
             <br>
             Add unregistered students
-            <v-layout row wrap>
-              <v-flex md2 xs6>
+            <v-row>
+              <v-col>
                 <v-text-field
                   label="SC/20XX/XXXX"
                   v-model="addUnreg">
-                </v-text-field>      
-              </v-flex>
-              <v-flex md4 xs6>
+                </v-text-field>     
+              </v-col>
+              <v-col>
                 <v-btn
                   @click="addUnregisteredStudents">
                   Add
                 </v-btn>
-              </v-flex>
-              <v-flex md4 xs6>
+              </v-col>
+              <v-col>
+                <v-btn
+                @click="e1=2">
+                  Previous
+                </v-btn>
+              </v-col>
+              <v-col>
                 <v-btn
                   color="primary"
-                  @click="e1 = 1">
+                  @click="uploadAttendance">
                   Confirm Attendance
                 </v-btn>
-              </v-flex>
-            </v-layout>
+              </v-col>
+            </v-row>
           </v-container>
         </v-stepper-content>
       </v-stepper-items>
     </v-stepper>
+    <v-snackbar
+    v-model="snackbar"
+    :timeout="2000">
+      {{ snackbarText }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="secondary"
+          text
+          v-bind="attrs"
+          @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-container>
 </template>
 
@@ -231,19 +290,20 @@ export default {
   data(){
     return{
       e1: 1,
+      isLoading: false,
+      snackbar: false,
+      snackbarText: '',
       //table header
       headers : markAttendance.headers,
 
       //stepper 3 props
       registeredStudents: [],
       registeredStudentsLength: 0,
-      recognizedStudents: [ 'SC/2017/10271' ],
+      recognizedStudents: [],
       absentStudents: [],
       addUnreg:'',
       removedStudent: '',
       presentStudents: '',
-      attendancePercentage: '',
-      datas: [],
       dialogDelete: false,
       dialog: null, //?????
 
@@ -254,8 +314,6 @@ export default {
       selectedType: null,
       selectedCourseCode: null,
       selectedLevel: null,
-      STenteredValue:'',
-      ETenteredValue: '',
       courses: [{
         co_id: "",
         course_code: "",
@@ -290,7 +348,7 @@ export default {
       // });
       //console.log(result);
       //stepper 3
-      // get courses list of the current lecture
+      // get courses list of the current lecturer
       const res = await axios.post(process.env.VUE_APP_BACKEND_SERVER + "/api/course/lecturer_id/",{
           lecturer_id,
       });
@@ -398,9 +456,23 @@ export default {
       // this.registeredStudentsLength = this.registeredStudents.length;
 
     },
+    async setRegisteredStudentData(){
+      this.isLoading = true;
+      // get registered student list for current course
+      const course_code = this.selectedCourseCode || this.courses[0].course_code;
+      const registeredStudentResult = await axios.post(process.env.VUE_APP_BACKEND_SERVER + "/api/student/course_code/",{
+          course_code,
+      });
+      //console.log(registeredStudentResult);
+      //return registered students
+      this.registeredStudents = registeredStudentResult.data.students;
+      this.isLoading = false;
+      this.e1 = 2;
+    },
     addUnregisteredStudents(){
-      this.recognizedStudents.push(this.addUnreg);
-      console.log(this.recognizedStudents)
+      if(this.addUnreg != '')
+        this.recognizedStudents.push(this.addUnreg);
+      //console.log(this.recognizedStudents)
     },
     //stepper 1 on change methods
     changedType(event) {
@@ -411,7 +483,7 @@ export default {
       }
     },
     changedLevel(event){
-      console.log(event);
+      //console.log(event);
       this.selectedLevel = event;
     },
     changedCourseCode(event){
@@ -420,31 +492,107 @@ export default {
     //stepper 2 camera methods
     onCapture() {  
       this.img = this.$refs.webcam.capture();
-      console.log(this.img);
+      //console.log(this.$refs.webcam.loadCameras());
+      // console.log(this.img);
       //var img; // get imagedata from POST request 
     },
     onCameras(cameras) {
       this.devices = cameras;
-      console.log("On Cameras Event", cameras);
     },    
     capture(){
       this.$refs.webcam.start();
       this.img = this.$refs.webcam.capture();
       // console.log(this.img);
-    },      
+    },
+    b64toBlob(b64Data, contentType, sliceSize) {
+        contentType = contentType || '';
+        sliceSize = sliceSize || 512;
+        var byteCharacters = atob(b64Data);
+        var byteArrays = [];
+        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            var slice = byteCharacters.slice(offset, offset + sliceSize);
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+            var byteArray = new Uint8Array(byteNumbers);
+            byteArrays.push(byteArray);
+        }
+      var blob = new Blob(byteArrays, {type: contentType});
+      return blob;
+    },   
+    async submitImage(){
+      this.isLoading = true;
+      var imageURL = this.img;
+      // Split the base64 string in data and contentType
+      var block = imageURL.split(";");
+      // Get the content type of the image
+      var contentType = block[0].split(":")[1];// In this case "image/jpeg"
+      // get the real base64 content of the file
+      var realData = block[1].split(",")[1];
+      // Convert it to a blob to upload
+      var blob = this.b64toBlob(realData, contentType);      
+      //create formdata object to send to server
+      let formData = new FormData();
+      formData.append('unknownImage', blob);
+      //this.isLoading = true;
+        await axios.post(process.env.VUE_APP_FLASK_SERVER+'/recognize', formData)
+        .then(result => {
+          //this.snackbar = true;
+          if(result.status===200) {
+            //console.log(result.data.names);
+            this.recognizedStudents = result.data.names;
+            // this.snackbarText = 'Image Saved Successfully!';
+            this.setStepper3Data();
+            this.isLoading = false;
+            this.e1 = 3;
+            this.img = null;
+          }
+          else {
+            // this.snackbarText = 'There was a Problem Saving the Image!';
+            console.log(result);
+            this.isLoading = false;
+          }
+          // this.isLoading = false;
+        }).catch(err => {
+          console.log(err);
+          this.isLoading = false;
+        });
+    },   
     // stepper 3 methods
-    deleteItem (item) {
-      this.editedIndex = this.absentStudents.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
-      this.removedStudent = item.registraion_no;
+    setStepper3Data(){     
+      // // logic for find absent student list
+      var flag,i,j;
+      for ( i = 0 ; i < this.registeredStudents.length; i++ ) {
+        flag = 0;
+        for (  j = 0; j < this.recognizedStudents.length;  j++ ) {
+          if(this.registeredStudents[i].registration_no ==  this.recognizedStudents[j]){
+            flag = 1;
+          }  
+        }
+        if(flag == 0){
+          this.absentStudents.push(this.registeredStudents[i]); 
+        }
+      }    
+      //Add unregistered
+      //find present students
+      this.presentStudents = this.registeredStudents.length - this.absentStudents.length;  
+      //find the attendance presentage
+      this.registeredStudentsLength = this.registeredStudents.length;
+    },
+    deleteItem (student) {
+      //console.log(student);
+      this.editedIndex = this.absentStudents.indexOf(student)
+      this.editedItem = Object.assign({}, student)
+      this.dialogDelete = true;
+      this.removedStudent = student.registration_no;
+      //console.log(this.removedStudent);
     },
     deleteItemConfirm () {
       this.absentStudents.splice(this.editedIndex, 1)
       this.closeDelete()
+      this.recognizedStudents.push(this.removedStudent);
       this.presentStudents++ ;
-      this.presentStudents.push(this.removedStudent);
-      console.log(this.presentStudents);
     },
     close () {
       this.dialog = false
@@ -460,6 +608,44 @@ export default {
         this.editedIndex = -1
       })
     },
+    async uploadAttendance () {
+      this.isLoading = true;
+      const course_code = this.selectedCourseCode || this.courses[0].course_code;
+      const type = this.selectedType || this.courses[0].type;
+      let absent_ids = this.absentStudents.map(s => {
+        return s.registration_no;
+      });
+      let co_id = this.courses.map(c => {
+        if(c.course_code == course_code && c.type == type)
+          return c.co_id;
+      });
+      co_id = co_id[0];
+      const registration_nos = this.recognizedStudents;
+
+      await axios.post(process.env.VUE_APP_BACKEND_SERVER + "/api/student/markattendance/",{
+      co_id,
+      type,
+      registration_nos,
+      absent_ids
+      })
+      .then(result => {
+        this.snackbar = true;
+        if(result.data.message==='success'){
+          this.snackbarText = 'Attendance Marked Successfully!';
+          this.e1 = 1;
+        }
+        else{
+          console.log(result.data.message);
+          this.snackbarText = 'There was an error Marking Attendance!';
+        }
+        this.isLoading = false;
+      })
+      .catch(err => {
+        console.log(err);
+        this.snackbarText = 'Attendance Marking Failed!';
+        this.isLoading = false;
+      })
+    }
   },
   watch: {
     //stepper 3
